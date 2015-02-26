08952db996d9cd28011f8818f00cb9b72dda19cd
zoness32 <tayloreernisse@gmail.com>
2015-02-26 15:21:56 -0500

    - Added PrefixIDs file - Renamed project to 'TerrariaFileParser' - Added files to handle rest of DB import workflow - Imported item ID info into DB - Imported prefix ID info into DB

--- terrariaItemParser/Program.cs
+++ terrariaItemParser/Program.cs
@@ -4,13 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace terrariaItemParser
+namespace TerrariaFileParser
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] lines = System.IO.File.ReadAllLines("C:\\Android\\projects\\TerrariaItemParser\\ItemIDs.txt");
+            Console.WriteLine("Please select the file you wish to edit:\n" +
+                "I)tem IDs\n" +
+                "P)refix IDs\n");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "I":
+                case "i":
+                    ParseItems();
+                    Console.WriteLine("Parsing complete. Exiting");
+                    break;
+                case "P":
+                case "p":
+                    ParsePrefixes();
+                    Console.WriteLine("Parsing complete. Exiting");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Exiting");
+                    break;
+            }
+            Console.Read();
+        }
+
+        private static void ParseItems()
+        {
+
+            string[] lines = System.IO.File.ReadAllLines("C:\\Android\\projects\\TerrariaFileParser\\ItemIDs.txt");
             for (int i = 1; i < lines.Length + 1; i++)
             {
                 string s = lines[i - 1];
@@ -18,7 +44,21 @@ namespace terrariaItemParser
                 lines[i - 1] = s;
                 Console.WriteLine(s);
             }
-            System.IO.File.WriteAllLines("C:\\Android\\projects\\TerrariaItemParser\\NewItemIDs.txt", lines);
+            System.IO.File.WriteAllLines("C:\\Android\\projects\\TerrariaFileParser\\NewItemIDs.txt", lines);
+        }
+
+        private static void ParsePrefixes()
+        {
+
+            string[] lines = System.IO.File.ReadAllLines("C:\\Android\\projects\\TerrariaFileParser\\PrefixIDs.txt");
+            for (int i = 1; i < lines.Length + 1; i++)
+            {
+                string s = lines[i - 1];
+                s = "" + i + "|" + s;
+                lines[i - 1] = s;
+                Console.WriteLine(s);
+            }
+            System.IO.File.WriteAllLines("C:\\Android\\projects\\TerrariaFileParser\\NewPrefixIDs.txt", lines);
         }
     }
 }
--- terrariaItemParser/Properties/AssemblyInfo.cs
+++ terrariaItemParser/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("terrariaItemParser")]
+[assembly: AssemblyTitle("TerrariaFileParser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("terrariaItemParser")]
+[assembly: AssemblyProduct("TerrariaFileParser")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
