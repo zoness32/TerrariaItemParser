c704dd10b1a09458e996b7db6302ebee5c4f8247
zoness32 <tayloreernisse@gmail.com>
2015-02-26 10:33:35 -0500

    -initial commit

---
+++ terrariaItemParser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace terrariaItemParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("C:\")
+        }
+    }
+}
